2a9215919339af0ea5d95ff51faefdfc3d3af49d
A. Bergiers <99248410+ChillinSoul@users.noreply.github.com>
2024-02-21 18:59:19 +0100

    downgraded to next 13

--- src/app/cmp/Card.tsx
+++ src/app/cmp/Card.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link"; // Import Link from next/link
 import CardInt from "@/types/Card";
 
@@ -6,11 +7,13 @@ const Card = ({ card }: { card: CardInt }) => {
   return (
     <li className="hover:scale-105">
       {/* Use Link for client-side navigation. Note that the `a` tag is still used but as a child of Link */}
-      <Link href={card.link} className="p-2 hover:bg-slate-500">
-        <div className="h-full w-full rounded border-2 border-purple-500 p-4 hover:bg-sky-700">
-          <h2 className="text-3xl text-purple-900">{card.title}</h2>
-          <p>{card.description}</p>
-        </div>
+      <Link legacyBehavior href={card.link} className="p-2">
+        <a>
+          <div className="h-full w-full rounded border-2 border-purple-500 p-4 hover:bg-purple-300">
+            <h2 className="text-3xl text-purple-900">{card.title}</h2>
+            <p>{card.description}</p>
+          </div>
+        </a>
       </Link>
     </li>
   );
--- src/app/cmp/CardBlock.tsx
+++ src/app/cmp/CardBlock.tsx
@@ -3,13 +3,7 @@ import Card from "./Card";
 
 export default function CardList({ cards }: CardIntList) {
   return (
-    <div className="card-list">
-      <div className="flex items-center space-x-4">
-        <a href="/"> Home</a>
-        <a href="/user"> Profile</a>
-        <a href="/login"> Login</a>
-        <a href="/settings">Settings</a>
-      </div>
+    <div className="w-full">
       <ul className="grid grid-cols-1 gap-4 md:grid-cols-3">
         {cards?.map((card: CardInt) => <Card key={card.id} card={card} />)}
       </ul>
