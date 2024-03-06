2094be8f9c2ad682cdf6032bfbc634122907738a
A. Bergiers <99248410+ChillinSoul@users.noreply.github.com>
2024-03-06 18:36:27 +0100

    laying grounds for the db

--- src/components/input/InputLabel.tsx
+++ src/components/input/InputLabel.tsx
@@ -2,7 +2,7 @@ import type { FormInputInt } from '../../types/Form';
 import { useState } from 'react';
 
 const InputLabel = ({label, name, type, color, value, maxWidth} : FormInputInt) => {
-    let label_style_string = `text-${color??"orange"}-400 rounded-lg   text-center`
+    let label_style_string = `text-${color??"orange"}-400 pr-2 text-center`
     let input_style_string = `border-2 text-slate-700 border-${color??"orange"}-400  rounded ${maxWidth == 'true' ? 'w-full' : ''} focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50`
 
     const [inputValue, setInputValue] = useState(value);
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.ts";
+
+
+async function startApolloServer() {
+    const server = new ApolloServer({ typeDefs });
+    const { url } = await startStandaloneServer(server);
+    console.log(`
+      🚀  Server is running!
+      📭  Query at ${url}
+    `);
+  }
+
+
+  startApolloServer();
